ccc443d179f0ee3d540ed43f8cd7e1a8dfdb083c
amehlhas <a.mehlhase@asu.edu>
2025-10-10 14:37:54 -0700

    had to fix

--- src/main/java/GameEngine.java
+++ src/main/java/GameEngine.java
@@ -29,10 +29,16 @@ public class GameEngine {
         if (guess == target) {
             gameWon = true;
             return new GuessResult(true, "Correct! You guessed it in " + attempts + " attempts.", attempts);
-        } else if (guess < target) {
-            return new GuessResult(false, "Too low! Try a higher number.", attempts);
         } else {
-            return new GuessResult(false, "Too high! Try a lower number.", attempts);
+            String hint = getHint(guess);
+            GuessResult result;
+            if (guess < target) {
+                result = new GuessResult(false, "Too low!", attempts);
+            } else {
+                result = new GuessResult(false, "Too high!", attempts);
+            }
+            result.setHint(hint);
+            return result;
         }
     }
 
